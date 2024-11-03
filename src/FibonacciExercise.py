dff65f9a3046dfbc41a6983952e90a5a7da87df4
DanMarBar <martinez.badan22@zaragoza.salesianos.edu>
2024-11-03 23:36:35 +0100

    Añadido constante de num de repeticiones

--- src/FibonacciExercise.py
+++ src/FibonacciExercise.py
@@ -1,10 +1,12 @@
+REPETITION_NUMBER = 20
+
 # Calcula la secuencia fibonacci, guarda los numeros en una lista y la regresa
 def calculate_fibonacci() -> list[int]:
     fibonacci_numbers_list: list[int] = []
     fibonacci_number: int = 0
     x: int = 1
 
-    for i in range(13):
+    for i in range(REPETITION_NUMBER):
         fibonacci_numbers_list.append(fibonacci_number)
         y = x
         x = fibonacci_number + x
